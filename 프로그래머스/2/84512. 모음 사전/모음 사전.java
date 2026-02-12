0763b4c33335802aa17d044dd1be17397406e5a9
Minji Kim <lovekmj23@gmail.com>
2026-02-12 11:05:54 +0900

    [level 2] Title: 모음 사전, Time: 0.05 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,7 +1,28 @@
 class Solution {
+
+    // 하드코딩 없이 가중치 생성: [781, 156, 31, 6, 1]
+    private int[] buildWeight() {
+        int[] weight = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            int sum = 0;
+            int pow = 1; // 5^0
+
+            // i번째 자리 뒤에 남은 길이: (5 - i)
+            for (int k = 0; k < 5 - i; k++) {
+                sum += pow;   // 1 + 5 + 25 + ...
+                pow *= 5;
+            }
+
+            weight[i] = sum;
+        }
+
+        return weight;
+    }
+
     public int solution(String word) {
+        int[] weight = buildWeight();
         int answer = 0;
-        int[] weight = {781, 156, 31, 6, 1};
 
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
